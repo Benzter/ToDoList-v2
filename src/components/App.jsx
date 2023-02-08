373ab4ce5b736f429f9b5682a8384994dde58760
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-02-08 11:31:00 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import ListItem from "./ListItem";
+
+function App() {
+  const [item, AddNewItem] = useState({ newItem: "", items: ["Hello"] });
+
+  function handleInput(event) {
+    const eventInput = event.target.value;
+    AddNewItem((preValue) => {
+      return {
+        ...preValue,
+        newItem: eventInput,
+      };
+    });
+  }
+
+  function handleSubmit() {
+    item.items.push(item.newItem);
+    AddNewItem((preValue) => {
+      return {
+        ...preValue,
+      };
+    });
+
+    item.newItem = "";
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" onChange={handleInput} value={item.newItem} />
+        <button type="submit" onClick={handleSubmit}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {/* {console.log(item.items)} */}
+          {item.items.map((newOne) => (
+            <ListItem item={newOne} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
